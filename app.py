be9315c7334e37b6d2ff80d31763bd2d65c81aa3
Mahima Chugh <34764608+mahima2112@users.noreply.github.com>
2023-04-18 01:20:09 +0000

    setup 2 dash apps

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ import sampleDashboard
 import pandas as pd
 import dash_html_components as html
 import plotly.express as px
-app = Flask(__name__)
+import flask
+app_flask = flask.Flask(__name__)
 
-@app.route("/")
+@app_flask.route('/abc') 
 def hello_world():
+    return flask.redirect('/pathname')
     # return render_template("index.html", title="Hello")
     # return sampleDashboard.getLayout()
-    import plotly.graph_objects as go
-    fig = go.Figure(
-        data=[go.Bar(y=[2, 1, 3])],
-        layout_title_text="A Figure Displayed with the bar chart"
-    )
-    fig.show()
+    # return 'Hello'
     
--- sampleDashboard.py
+++ sampleDashboard.py
@@ -1,38 +1,74 @@
-import pandas as pd
-import dash_html_components as html
-import plotly.express as px
-import dash_core_components as dcc
-from dash.dependencies import Input, Output, State
+# import pandas as pd
+# import dash_html_components as html
+# import plotly.express as px
+# import dash_core_components as dcc
+# from dash.dependencies import Input, Output, State
+# import dash
+# import flask
+
+# app_flask = flask.Flask(__name__)
+
+# app_dash = dash.Dash(__name__, server=app_flask, url_base_pathname='/pathname')
+
+# class sampleDashboard:
+#     dashData = {}
+
+#     def load_data_frame(self):
+#         data = [{'nation':'South Korea', 'medal': 'gold', 'count': 24}]
+#         return pd.DataFrame(data)
+
+
+# sample_dashboard = sampleDashboard()
+
+
+# def getLayout():
+#     # df = sample_dashboard.load_data_frame()
+
+#     return [
+#                     html.Div(children=[
+#                             dcc.Graph(id='graph')
+#                     ])
+#     ]
+
+# @app_flask.callback(Output('graph', 'figure'),
+#               Input('url', 'search'),
+#               State('url', 'pathname'))
+# def update_figure(search, pathname):
+#     df = sample_dashboard.load_data_frame()
+#     fig = {}
+#     if not df.empty:
+#         fig = px.bar(df, x="nation", y="count", title="graph")
+#     return fig
+
+
 from dash import Dash
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+import flask
+from werkzeug.serving import run_simple
+import dash_html_components as html
 
-app = Dash(__name__)
+server = flask.Flask(__name__)
+dash_app1 = Dash(__name__, server = server, url_base_pathname='/dashboard/' )
+dash_app2 = Dash(__name__, server = server, url_base_pathname='/reports/')
+dash_app1.layout = html.Div([html.H1('Hi there, I am app1 for dashboards')])
+dash_app2.layout = html.Div([html.H1('Hi there, I am app2 for reports')])
+@server.route('/')
+@server.route('/hello')
+def hello():
+    return 'hello world!'
 
-class sampleDashboard:
-    dashData = {}
-
-    def load_data_frame(self):
-        data = [{'nation':'South Korea', 'medal': 'gold', 'count': 24}]
-        return pd.DataFrame(data)
+@server.route('/dashboard/')
+def render_dashboard():
+    return flask.redirect('/dash1')
 
 
-sample_dashboard = sampleDashboard()
+@server.route('/reports/')
+def render_reports():
+    return flask.redirect('/dash2')
 
+app = DispatcherMiddleware(server, {
+    '/dash1': dash_app1.server,
+    '/dash2': dash_app2.server
+})
 
-def getLayout():
-    # df = sample_dashboard.load_data_frame()
-
-    return [
-                    html.Div(children=[
-                            dcc.Graph(id='graph')
-                    ])
-    ]
-
-@app.callback(Output('graph', 'figure'),
-              Input('url', 'search'),
-              State('url', 'pathname'))
-def update_figure(search, pathname):
-    df = sample_dashboard.load_data_frame()
-    fig = {}
-    if not df.empty:
-        fig = px.bar(df, x="nation", y="count", title="graph")
-    return fig
+run_simple('0.0.0.0', 8080, app, use_reloader=True, use_debugger=True)
